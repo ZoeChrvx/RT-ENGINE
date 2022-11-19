9d4d2799947fc280c76e16e7af4697eb31f48127
ZoeChrvx <zchervaux@artfx.fr>
2022-11-19 20:58:52 +0100

    [fix] Bug with collider

--- Assets/Script/MainScene/EnemyLife.cs
+++ Assets/Script/MainScene/EnemyLife.cs
@@ -4,25 +4,10 @@ using UnityEngine;
 
 public class EnemyLife : MonoBehaviour
 {
-    public List<GameObject> drop;
-    public int enemy = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void DeathEnemy()
+        public void DeathEnemy()
     {
         Debug.Log("Cheh");
-        enemy--;
         GetComponent<LootBag>().InstantiateLoot(transform.position);
         Destroy(gameObject);
-        
     }
 }
