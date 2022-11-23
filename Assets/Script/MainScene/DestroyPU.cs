4c952d2ecc2a4d169af0b1ea3cb406847c023e5c
ZoeChrvx <zchervaux@artfx.fr>
2022-11-23 12:50:43 +0100

    [add] Some few things

--- Assets/Script/MainScene/DestroyPU.cs
+++ Assets/Script/MainScene/DestroyPU.cs
@@ -6,7 +6,7 @@ public class DestroyPU : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Collectable" || collision.tag == "Powerup0" || collision.tag == "Powerup1" || collision.tag == "Powerup2" )
+        if (collision.tag == "Collectable" || collision.tag == "Powerup" || collision.tag == "Powerup1" || collision.tag == "Powerup2" )
         {
             Destroy(collision.gameObject);
             Debug.Log("Just destroy");
--- Assets/Script/OtherScene/DeathResume.cs
+++ Assets/Script/OtherScene/DeathResume.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class DeathResume : MonoBehaviour
 {
-    public TextMeshProUGUI finalScore;
-    
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    
+    // Update is called once per frame
     void Update()
     {
         
