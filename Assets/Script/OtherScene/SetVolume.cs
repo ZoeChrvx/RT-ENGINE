0b5ff5d67730051f9e68f5bf46819bf114c139f8
ZoeChrvx <zchervaux@artfx.fr>
2022-11-27 18:37:19 +0100

    [add] Volume and do some test

---
+++ Assets/Script/OtherScene/SetVolume.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVolume : MonoBehaviour
+{
+    public void VolumeSlider(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
